9433769f92fee2b38b2db6c461e2dc63f5405ffd
Jessica Poe <jesspoe00@gmail.com>
2019-07-18 19:21:56 -0700

    created an override using Deface

---
+++ app/overrides/update_footer.rb
@@ -0,0 +1,6 @@
+# Deface:: Override.new(virtual_path => 'spree/shared/_footer', 
+# :name => 'change footer phone number',
+# :replace => 'div.phone',
+# :text => <div class="phone">
+            # 321-863-5375 </div>
+# )
