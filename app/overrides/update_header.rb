30945e8cf41f2013dff80d7b9d72f57f86c8ce54
Jessica Poe <jesspoe00@gmail.com>
2019-07-18 20:58:48 -0700

    created a tagline and edited placement

--- app/overrides/update_header.rb
+++ app/overrides/update_header.rb
@@ -2,5 +2,5 @@ Deface:: Override.new(:virtual_path => 'spree/shared/_header',
 :name => 'add text to header',
 :insert_after => 'figure#logo',
 :text => '<div class="header-text">
-The best gift for vegans! </div>'
+Great Vegan Gifts! </div>'
 )
